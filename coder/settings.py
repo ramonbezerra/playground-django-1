230d6e0d2acad28c33395997fec55a3685a6b4ca
ramonbezerra <ramon.bezerra@outlook.com>
2025-02-10 21:13:48 -0300

    feat: include base template

--- coder/settings.py
+++ coder/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'coder.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'courses\\templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'coder\\templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- courses/urls.py
+++ courses/urls.py
@@ -2,7 +2,15 @@ from django.urls import include, path
 
 from courses import views
 
-urlpatterns = [
+appname = 'courses'
+
+patterns = [
     path('', views.courses_list, name='courses_list'),
     path('<int:course_id>', views.course_detail, name='course_detail'),
+]
+
+courses_patterns = (patterns, appname)
+
+urlpatterns = [
+    path('', include(courses_patterns)),
 ]
