f8df65f4ecfa4c76bb459247a6dcf7801c7a3553
ramonbezerra <ramon.bezerra@outlook.com>
2025-02-17 21:34:56 -0300

    feat: create project

--- projects/urls.py
+++ projects/urls.py
@@ -7,6 +7,7 @@ appname = 'projects'
 patterns = [
     path('', views.projects_list, name='projects_list'),
     path('<int:project_id>', views.project_detail, name='project_detail'),
+    path('create/', views.create_project, name='create_project'),
 ]
 
 projects_patterns = (patterns, appname)
